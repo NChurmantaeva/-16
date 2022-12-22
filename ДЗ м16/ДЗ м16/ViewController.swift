71795559eaaf6ffd8f7c4f308b01ce20a7a9bd96
Nadezhda Churmantaeva <askenderova.nadya@gmail.com>
2022-12-22 22:59:08 +0300

    Initial Commit. Created empty project

---
+++ ДЗ м16/ДЗ м16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ДЗ м16
+//
+//  Created by Надежда Чурмантаева on 22.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
